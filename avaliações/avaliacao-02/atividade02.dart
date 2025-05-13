e7abcd3a51a9d1224593c0a4a6800eba5dda17d7
João Marcelo <159565540+jmcolombini@users.noreply.github.com>
2025-05-13 14:52:17 -0300

    Add files via upload

---
+++ avaliações/avaliacao-02/atividade02.dart
@@ -0,0 +1,67 @@
+// Agregação e Composição
+import 'dart:convert';
+
+class Dependente {
+  late String _nome;
+
+  Dependente(String nome) {
+    this._nome = nome;
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'nome': _nome,
+    };
+  }
+}
+
+class Funcionario {
+  late String _nome;
+  late List<Dependente> _dependentes;
+
+  Funcionario(String nome, List<Dependente> dependentes) {
+    this._nome = nome;
+    this._dependentes = dependentes;
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'nome': _nome,
+      'dependentes': _dependentes.map((d) => d.toJson()).toList(),
+    };
+  }
+}
+
+class EquipeProjeto {
+  late String _nomeProjeto;
+  late List<Funcionario> _funcionarios;
+
+  EquipeProjeto(String nomeprojeto, List<Funcionario> funcionarios) {
+    _nomeProjeto = nomeprojeto;
+    _funcionarios = funcionarios;
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'nomeProjeto': _nomeProjeto,
+      'funcionarios': _funcionarios.map((f) => f.toJson()).toList(),
+    };
+  }
+}
+
+void main() {
+  var dep1 = Dependente("Ana");
+  var dep2 = Dependente("Bruno");
+  var dep3 = Dependente("Carlos");
+
+  var func1 = Funcionario("João", [dep1, dep2]);
+  var func2 = Funcionario("Maria", [dep3]);
+
+  var funcionarios = [func1, func2];
+
+  var equipe = EquipeProjeto("Projeto Alpha", funcionarios);
+
+  var jsonEquipe = jsonEncode(equipe.toJson());
+  
+  print(jsonEquipe);
+}
